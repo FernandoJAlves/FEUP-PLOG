0134b40a59a67a309f99d32ff5ca38d49a5e3a6f
Fernando Alves <up201605270@fe.up.pt>
2018-11-18 13:24:08 +0000

    Added 8 directional value

--- Proj/utilities.pl
+++ Proj/utilities.pl
@@ -79,8 +79,8 @@ scoreLine(_,2,OutValue) :- OutValue is 0.
 scoreLine(_,3,OutValue) :- OutValue is 0.
 scoreLine(_,4,OutValue) :- OutValue is 0.
 scoreLine(1,0,OutValue) :- OutValue is 1.
-scoreLine(2,0,OutValue) :- OutValue is 1.
-scoreLine(3,0,OutValue) :- OutValue is 1.
+scoreLine(2,0,OutValue) :- OutValue is 10.
+scoreLine(3,0,OutValue) :- OutValue is 50.
 scoreLine(4,0,OutValue) :- OutValue is 100.
 scoreLine(5,0,OutValue) :- OutValue is 10000.
 
@@ -90,15 +90,15 @@ ifElse(C,G,E) :- E.
 
 setupTestTab(Tab) :-
 	storeCell(blackStone,5,2),
-	storeCell(blackStone,4,2),
 	storeCell(blackStone,5,3),
-	storeCell(blackStone,4,3),
-	storeCell(blackStone,4,4),
+	storeCell(blackStone,4,2),
+	storeCell(whiteStone,4,3),
+	storeCell(whiteStone,6,3),
 	Tab = [[emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace],
 			[emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace],
-			[emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace],
+			[emptySpace,emptySpace,whiteStone,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace],
 			[emptySpace,blackStone,blackStone,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace],
-			[emptySpace,blackStone,blackStone,blackStone,emptySpace,emptySpace,emptySpace,emptySpace],
+			[emptySpace,blackStone,whiteStone,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace],
 			[emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace],
 			[emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace],
 			[emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace,emptySpace]].
